3e1f4b028ef5040be78ca2cb615c8ad1e4efe19f
Mary Cods <maryam.selassie@gmail.com>
2023-02-05 20:40:42 -0800

    Final Commit

--- Develop/script.js
+++ Develop/script.js
@@ -1,58 +1,73 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var dayBlock = document.getElementById('.blocked-day');
 
+var dayBlocks = $('.time-block');
 
-var now =  dayjs().hour(10)
+var lockEl = $('.saveBtn');
+
+ var now = dayjs().format('dddd, MMM D, YYYY')
+
 //  dayjs().format('MMM D'); 
-  $('#currentDay').text(now);
+$('#currentDay').text(now);
 
- function dayTimer() {
-       //var timeRightNow =  dayjs("2020-24").hour()
-       var timeRightNow =  dayjs()
-  dayBlock.each(function () {
-      var blockedDay = attr('id').parseInt('.hour-');
-        
-       if (blockedDay === timeRightNow){
-        dayBlock.removeClass('past');
-        dayBlock.removeClass('future');
-         dayBlock.addClass('present');
-        }
+function dayTimer() {
+  //var timeRightNow =  dayjs("2020-24").hour()
+  var timeRightNow = dayjs().hour()
+  // console.log(timeRightNow);
+  dayBlocks.each(function () {
+    var blockedDay = $(this).attr('id').split('-')[1]
+    //console.log(this)
 
-       else if (blockedDay < timeRightNow){
-        dayBlock.removeClass('present');
-        dayBlock.removeClass('future');
-        dayBlock.addClass('past');
-      }
+    if (parseInt(blockedDay) === timeRightNow) {
+      $(this).removeClass('past');
+      $(this).removeClass('future');
+      $(this).addClass('present');
+    }
 
-        else {
-         dayBlock.removeClass('present');
-       dayBlock.removeClass('past');
-        dayBlock.addClass('future');
-       }
-  
-   
+    else if (blockedDay < timeRightNow) {
+      $(this).removeClass('present');
+      $(this).removeClass('future');
+      $(this).addClass('past');
+    }
+
+    else {
+      $(this).removeClass('present');
+      $(this).removeClass('past');
+      $(this).addClass('future');
+    }
+
+    
   });
 };
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
- 
+dayTimer();
+
+function save() {
+  var clickedEl = $(this);
+  var description = clickedEl.siblings('.description').val();
+  var time = clickedEl.parent().attr('id');
+  console.log(description, time);
+  // var hourlySchedule =[];
+  // var alreadySavedItiems = localStorage.getItem('Hourly-Schedule');
+  // if(alreadySavedItiems){
+  //   (hourlySchedule) = JSON.parse(alreadySavedItiems);
+  // }
+
+  // hourlySchedule[clickedEl.parent().attr('id')] = daySchedule;
+  localStorage.setItem(time, description);
+  
+}
+
+$('#hour-9 .description').val(localStorage.getItem('hour-9'))
+
+$('#hour-10 .description').val(localStorage.getItem('hour-10'))
+
+$('#hour-11 .description').val(localStorage.getItem('hour-11'))
+
+$('#hour-12 .description').val(localStorage.getItem('hour-12'))
+
+$('#hour-13 .description').val(localStorage.getItem('hour-13'))
+
+$('#hour-14 .description').val(localStorage.getItem('hour-14'))
+
+$('#hour-15 .description').val(localStorage.getItem('hour-15'))
+lockEl.on('click', save);
   
  
